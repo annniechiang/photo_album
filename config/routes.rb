0c0b0bb307b2b52a66c483f3bd93dfd7204b1069
annie chiang <annie100patty@gmail.com>
2018-01-29 14:38:47 +0800

    setup user resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :photo2s
   root "photo2s#index"
+  resources :photo2s
+  resources :users
 
 end
